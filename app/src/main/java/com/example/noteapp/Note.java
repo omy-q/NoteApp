90ca64a2b5958dabb0904df4b368353a86a956ef
omy-q <olya_aparina@mail.ru>
2021-06-20 01:53:40 +0300

    implements Parcelable to Note class

--- app/src/main/java/com/example/noteapp/Note.java
+++ app/src/main/java/com/example/noteapp/Note.java
@@ -1,6 +1,9 @@
 package com.example.noteapp;
 
-public class Note {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
 
     private String noteName;
     private String noteDate;
@@ -12,27 +15,43 @@ public class Note {
         this.noteDate = noteDate;
     }
 
+    protected Note(Parcel in) {
+        noteName = in.readString();
+        noteDescription = in.readString();
+        noteDate = in.readString();
+    }
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
     public String getNoteName() {
         return noteName;
-    }
-
-    public void setNoteName(String noteName) {
-        this.noteName = noteName;
     }
 
     public String getNoteDescription() {
         return noteDescription;
     }
 
-    public void setNoteDescription(String noteDescription) {
-        this.noteDescription = noteDescription;
-    }
-
     public String getNoteDate() {
         return noteDate;
     }
 
-    public void setNoteDate(String noteDate) {
-        this.noteDate = noteDate;
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getNoteName());
+        dest.writeString(getNoteDescription());
+        dest.writeString(getNoteDate());
     }
 }
