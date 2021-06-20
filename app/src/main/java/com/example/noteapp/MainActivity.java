9565db9b8fa82768c769b7040473e215dd8b4a85
omy-q <olya_aparina@mail.ru>
2021-06-20 12:34:44 +0300

    init Fragment in MainActivity

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFragment();
+    }
+
+    private void initFragment() {
+        NotesListFragment notesListFragment = new NotesListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.notes_list, notesListFragment);
+        fragmentTransaction.commit();
     }
 }
